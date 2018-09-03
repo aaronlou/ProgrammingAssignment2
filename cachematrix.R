29cb11b61bb458bcf13705b273d039c29e9b4a34
aaron <siyuanlou@me.com>
2018-09-03 20:33:25 +0800

    modify cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    #initialize a matrix
     revM <- NULL
+    
+    #set is used to give a specified input value
     set <- function(y){
         x <<- y
         revM <<- NULL
     }
+    
+    #return the matrix which we want to solove
     get <- function() x
+    
+    #we could also directly give a inversed matrix
     setRevM <- function(solve) revM <<- solve
+    
+    #return  the inversed matrix
     getRevM <- function() revM
     list(set=set,get=get,setRevM=setRevM,getRevM=getRevM)
 }
@@ -20,11 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    # test if the inversed matrix of input x does already exist
     revM <- x$getRevM()
-    if(class(revM) == "matrix") {
+    if(!is.null(revM)) {
         message("getting cached data")
+        #if we really have, just return it, and this function stop here
         return(revM)
     }
+    
+    #if not, we need to solve it manually
     data <- x$get()
     revM <- solve(data, ...)
     x$setRevM(revM)
